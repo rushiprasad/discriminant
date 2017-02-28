1e51dbfdd5d8cd0dbf640b1f35bff952c92ed0e0
Rushi <rushi@localhost.localdomain>
2017-02-28 11:37:49 +0530

    discriminant

---
+++ c.py
@@ -0,0 +1,11 @@
+print "------------------------------------------------------"
+print "Enter value of a,b,c to find value of discriminant"
+a = int(raw_input("Enter the value of a: "))
+b = int(raw_input("Enter the value of b: "))
+c = int(raw_input("Enter the value of c: "))
+
+
+d = b ** 2 -  4 * a * c
+
+
+print "determinant = ",d
